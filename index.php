f3fc902aa75d7fb83450b11e995d0272fc079f07
Zhuravlev Denis <zhuravlevdenis94@gmail.com>
2017-06-19 01:18:49 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajax Database</title>
+</head>
+<body>
+    Name: <input type="text" id="name">
+    <input type="submit" id="name-submit" value="ok">
+    <div id="name-data"></div>
+
+    <script src="https://code.jquery.com/jquery-1.8.0.min.js"></script>
+    <script src="js/global.js"></script>
+</body>
+</html>
